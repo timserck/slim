3ea18d0e37030bfa04fbadb1048b55f96d405277
timserck <timserck@gmail>
2015-10-21 20:52:23 +0200

    return json
    testing index php return json

---
+++ index.php
@@ -0,0 +1,18 @@
+    <?php
+
+     require 'vendor/autoload.php';
+
+    $app = new \Slim\Slim();
+
+    $app->view(new \JsonApiView());
+    $app->add(new \JsonApiMiddleware());
+
+     $app->get('/', function() use ($app) {
+        $app->render(200,array(
+                'msg' => 'Welcome to my json API!',
+            ));
+    });
+
+     header("Content-Type: application/json"); 
+   echo json_encode($app['message']); 
+   exit;
